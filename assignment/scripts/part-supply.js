d861136d221c9c9ac13755851b17f5ee347707f3
Dave Nash <davidjnash2@gmail.com>
2023-04-14 08:31:43 -0500

    answered eight

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -20,8 +20,7 @@ console.log(supplyChanges[1]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
-let removedItem = 11;
-supplyChanges.pop();
+let removedItem = supplyChanges.pop();
 console.log(removedItem);
 console.log(supplyChanges);
 
@@ -37,16 +36,14 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-for ( x=0; x<supplyChanges.length; x++){
-    console.log(supplyChanges[x]);
-    if (supplyChanges[x]>0){
-        console.log('Added ', supplyChanges[x], ' parts.');
-    }
-    if (supplyChanges[x]==0){
+for ( i=0; i<supplyChanges.length; i++){
+    console.log(supplyChanges[i]);
+    if (supplyChanges[i]>0){
+        console.log('Added ', supplyChanges[i], ' parts.');
+    } else if (supplyChanges[i]==0){
         console.log('No change.');
-    }
-    if (supplyChanges[x]<0){
-        console.log('Removed ', supplyChanges[x], ' parts.');
+    } else if (supplyChanges[i]<0){
+        console.log('Removed ', supplyChanges[i], ' parts.');
     }
 }
 
@@ -55,16 +52,14 @@ for ( x=0; x<supplyChanges.length; x++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-for ( x of supplyChanges){
-    console.log(x);
-    if (x > 0){
-        console.log('Added ', x, ' parts.');
-    }
-    if (x===0){
+for ( i of supplyChanges){
+    console.log(i);
+    if (i > 0){
+        console.log('Added ', i, ' parts.');
+    } else if (i===0){
         console.log('No change.');
-    }
-    if (x<0){
-        console.log('Removed ', x, ' parts.');
+    } else if (i<0){
+        console.log('Removed ', i, ' parts.');
     }
 }
 
@@ -73,6 +68,14 @@ for ( x of supplyChanges){
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+    let sum = 0 
+    for( i=0; i<supplyChanges.length; i++){
+        sum += supplyChanges[i];
+    }
+    console.log(sum);
+
+
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
